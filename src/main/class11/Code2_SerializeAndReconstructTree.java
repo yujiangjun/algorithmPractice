d70715511894d5936a3bd9241e61c560a37670f1
余江俊 <18512106361@163.com>
2021-12-01 21:24:46 +0800

    树的序列化和反序列化

--- src/main/class11/Code2_SerializeAndReconstructTree.java
+++ src/main/class11/Code2_SerializeAndReconstructTree.java
@@ -120,4 +120,141 @@ public class Code2_SerializeAndReconstructTree {
         head.left=posb(posStack);
         return head;
     }
+
+    public static Queue<String> levelSerial(Node head){
+        Queue<String> ans = new LinkedList<>();
+        if (head==null){
+            ans.add(null);
+        }else {
+            ans.add(String.valueOf(head.value));
+            Queue<Node> queue=new LinkedList<>();
+            queue.add(head);
+            while (!queue.isEmpty()){
+                head=queue.poll();
+                if (head.left!=null){
+                    ans.add(String.valueOf(head.left.value));
+                    queue.add(head.left);
+                }else {
+                    ans.add(null);
+                }
+                if (head.right!=null){
+                    ans.add(String.valueOf(head.right.value));
+                    queue.add(head.right);
+                }else {
+                    ans.add(null);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static Node buildByLevelQueue(Queue<String> levelList){
+        if (levelList==null||levelList.isEmpty()){
+            return null;
+        }
+        Node head=generateNode(levelList.poll());
+        Queue<Node> queue=new LinkedList<>();
+        if (head!=null){
+            queue.add(head);
+        }
+        Node node=null;
+        while (!queue.isEmpty()){
+            node=queue.poll();
+            node.left=generateNode(levelList.poll());
+            node.right=generateNode(levelList.poll());
+            if (node.left!=null){
+                queue.add(node.left);
+            }
+            if (node.right!=null){
+                queue.add(node.right);
+            }
+        }
+        return head;
+    }
+
+    public static Node generateNode(String val){
+        if (val==null){
+            return null;
+        }
+        return new Node(Integer.valueOf(val));
+    }
+
+    public static Node generateRandomBST(int maxLevel,int maxValue){
+        return generate(1,maxLevel,maxValue);
+    }
+    public static Node generate(int level,int maxLevel,int maxValue){
+        if (level>maxLevel|| Math.random()<0.5){
+            return null;
+        }
+        Node head = new Node((int) (Math.random()*maxValue));
+        head.left=generate(level+1,maxLevel,maxValue);
+        head.right=generate(level+1,maxLevel,maxValue);
+        return head;
+    }
+
+    public static boolean isSameValueStructure(Node head1,Node head2){
+        if (head1==null&&head2!=null){
+            return false;
+        }
+        if (head1!=null&&head2==null){
+            return false;
+        }
+        if (head1==null&&head2==null){
+            return true;
+        }
+        if (head1.value!= head2.value){
+            return false;
+        }
+        return isSameValueStructure(head1.left, head2.left)&&isSameValueStructure(head1.right,head2.right);
+    }
+
+    public static void printTree(Node head){
+        System.out.println("Binary Tree:");
+        printInOrder(head,0,"H",17);
+        System.out.println();
+    }
+
+    public static void printInOrder(Node head,int height,String to,int len){
+        if (head==null){
+            return;
+        }
+        printInOrder(head.right,height+1,"v",len);
+        String val=to+head.value+to;
+        int lenM=val.length();
+        int lenL=(len-lenM)/2;
+        int lenR=len-lenM-lenL;
+        val=getSpace(lenL)+val+getSpace(lenR);
+        System.out.println(getSpace(height*len)+val);
+        printInOrder(head.left,height+1,"^",len);
+    }
+
+    public static String getSpace(int num){
+        String space=" ";
+        StringBuffer buf=new StringBuffer("");
+        for (int i = 0; i < num; i++) {
+            buf.append(space);
+        }
+        return buf.toString();
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 5;
+        int maxValue = 100;
+        int testTimes = 1000000;
+        System.out.println("test begin");
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            Queue<String> pre = perSerial(head);
+            Queue<String> pos = posSerial(head);
+            Queue<String> level = levelSerial(head);
+            Node preBuild = buildByPreQueue(pre);
+            Node posBuild = buildByPosQueue(pos);
+            Node levelBuild = buildByLevelQueue(level);
+            if (!isSameValueStructure(preBuild, posBuild) || !isSameValueStructure(posBuild, levelBuild)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("test finish!");
+
+    }
 }
