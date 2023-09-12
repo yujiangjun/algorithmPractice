4cb8325cae17d4411164f57110d0a03946eff7f9
yujiangjun <18512106361>
2023-09-12 10:33:49 +0800

    class15[code01,code02]

---
+++ src/main/dachang/class15/Code01_BestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+package dachang.class15;
+
+public class Code01_BestTimeToBuyAndSellStock {
+
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int ans = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            min = Math.min(min, prices[i]);
+            ans = Math.max(prices[i] - min, ans);
+        }
+        return ans;
+    }
+}
